41f5e5dec4ff8723c4b0be8c8eb2bfa5eb7dfc03
Nicholas Defin J. <mayagareyuki12@gmail.com>
2025-07-15 15:36:36 +0700

    Menambahkan FranchiorProfile

--- App.js
+++ App.js
@@ -1,13 +1,8 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-
+import FranchiseProfile from './screens/FranchiseProfile';
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  return <FranchiseProfile />;
 }
 
 const styles = StyleSheet.create({
